2d4655262063f79015ac4156aef5f85ca7de7d8d
Caryl Wyatt <carylw@hathitrust.org>
2023-05-16 13:02:00 -0500

    change all the modals

--- src/js/components/NotificationsModal/index.svelte
+++ src/js/components/NotificationsModal/index.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { onMount } from 'svelte';
+  import { onMount } from 'svelte';
   import Modal from '../Modal';
 
   export let manager;
@@ -7,30 +7,65 @@
 
   export let isOpen = false;
 
-  export const show = function() {
-    if ( ! manager.hasNotifications() ) { return ; }
+  export const show = function () {
+    if (!manager.hasNotifications()) {
+      return;
+    }
     modal.show();
-  }
+  };
 
-  export const hide = function() {
+  export const hide = function () {
     modal.hide();
-  }
+  };
 
-  const onClose = function() {
+  const onClose = function () {
     manager.updateTimestamp();
-  }
+  };
 
   onMount(() => {
-    if ( modal && manager.hasNewNotifications() ) {
+    if (modal && manager.hasNewNotifications()) {
       modal.show();
     }
-  })
+  });
 
-  $: if ( modal && manager.hasNewNotifications() ) { show() }
-  $: if ( modal && isOpen ) { show(); }
+  $: if (modal && manager.hasNewNotifications()) {
+    show();
+  }
+  $: if (modal && isOpen) {
+    show();
+  }
   // $: if ( modal && ! isOpen ) { hide() }
-
 </script>
+
+{#if manager.hasNotifications()}
+  <Modal bind:this={modal} {onClose}>
+    <svelte:fragment slot="title">Your notifications</svelte:fragment>
+    <svelte:fragment slot="body">
+      <ul class="list-group list-group-flush">
+        {#each manager.notificationData as datum, i}
+          <li class="list-group-item p-3">
+            <h2 class="fs-6">{datum.title}</h2>
+            {#if datum.message.indexOf('<p>') > -1}
+              {@html datum.message}
+              <p>
+                <a href={datum.read_more_link} target="_blank"
+                  >{datum.read_more_label}</a
+                >
+              </p>
+            {:else}
+              <p>
+                {datum.message}
+                <a href={datum.read_more_link} target="_blank"
+                  >{datum.read_more_label}</a
+                >
+              </p>
+            {/if}
+          </li>
+        {/each}
+      </ul>
+    </svelte:fragment>
+  </Modal>
+{/if}
 
 <style>
   pre {
@@ -39,27 +74,3 @@
     right: 1rem;
   }
 </style>
-
-{#if manager.hasNotifications()}
-<Modal bind:this={modal} onClose={onClose}>
-  <svelte:fragment slot="modal-title">Your notifications</svelte:fragment>
-  <svelte:fragment slot="modal-body">
-    <ul class="list-group list-group-flush">
-      {#each manager.notificationData as datum, i}
-        <li class="list-group-item p-3">
-          <h2 class="fs-6">{datum.title}</h2>
-          {#if datum.message.indexOf('<p>') > -1}
-            {@html datum.message}
-            <p><a href="{datum.read_more_link}" target="_blank">{datum.read_more_label}</a></p>
-          {:else}
-            <p>
-              {datum.message}
-              <a href="{datum.read_more_link}" target="_blank">{datum.read_more_label}</a>
-            </p>
-          {/if}          
-        </li>
-      {/each}
-    </ul>
-  </svelte:fragment>
-</Modal>
-{/if}
