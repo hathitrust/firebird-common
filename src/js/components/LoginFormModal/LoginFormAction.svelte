61bbfaaebb94fe86351c4dbf38aba4fa5d4264e4
Roger Espinosa <roger@umich.edu>
2023-06-22 17:48:21 -0400

    fix modal actions

--- src/js/components/LoginFormModal/LoginFormAction.svelte
+++ src/js/components/LoginFormModal/LoginFormAction.svelte
@@ -16,6 +16,7 @@
     let selected = HT.login_status.idp_list.find(
       (item) => item.sdrinst == sdrinst
     );
+    console.log("-- handleClick", sdrinst, selected);
     if (selected) {
       HT.prefs.set({ sdrinst: sdrinst });
       let login_href = selected.idp_url.replace(
@@ -29,7 +30,7 @@
 </script>
 
 <div class="m-0 w-100">
-  <div class="p-3 px-modal mb-2 d-flex gap-5 justify-content-between">
+  <div class="p-3 px-modal mb-2 d-flex flex-wrap flex-lg-nowrap gap-3 gap-lg-5 justify-content-between">
     <p class="fs-7 m-0">
       By logging into HathiTrust, you agree to follow our
       <a href="https://hathitrust.org/the-collection/access-use-policy/"
