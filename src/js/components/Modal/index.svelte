f56c6acc9f11f0a77e14e2bc0b5ea26e49634e52
Roger Espinosa <roger@umich.edu>
2023-06-28 21:15:48 -0400

    if the modal is displayed but not inert it steals events + focus styles

--- src/js/components/Modal/index.svelte
+++ src/js/components/Modal/index.svelte
@@ -113,26 +113,26 @@
     transition: opacity 0.25s var(--ease-3);
   }
 
-  // dialog:not([open]) {
-  //   display: none;
-  // }
-
   dialog:not([open]) {
-    pointer-events: none;
-    opacity: 0;
-    z-index: -100;
-
-    // for browsers that do not support inert
-    position: absolute !important;
-    width: 1px !important;
-    height: 1px !important;
-    padding: 0 !important;
-    margin: -1px !important;
-    overflow: hidden !important;
-    clip: rect(0, 0, 0, 0) !important;
-    white-space: nowrap !important;
-    border: 0 !important;
+    display: none;
   }
+
+  // dialog:not([open]) {
+  //   pointer-events: none;
+  //   opacity: 0;
+  //   z-index: -100;
+
+  //   // for browsers that do not support inert
+  //   position: absolute !important;
+  //   width: 1px !important;
+  //   height: 1px !important;
+  //   padding: 0 !important;
+  //   margin: -1px !important;
+  //   overflow: hidden !important;
+  //   clip: rect(0, 0, 0, 0) !important;
+  //   white-space: nowrap !important;
+  //   border: 0 !important;
+  // }
 
   dialog::backdrop {
     transition: backdrop-filter 0.25s ease;
