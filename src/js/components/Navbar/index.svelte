e430ad4c767e2ff79bc85cc0425ddbe9b842ccd7
Roger Espinosa <roger@umich.edu>
2023-06-29 14:20:31 -0400

    make openLogin even simpler

--- src/js/components/Navbar/index.svelte
+++ src/js/components/Navbar/index.svelte
@@ -38,12 +38,7 @@
 
   function openLogin() {
     //check viewport size to see if LoginFormModal will fit
-    const ratio = window.innerWidth / window.innerHeight;
-    console.log("-- open.login", ratio, window.innerWidth, window.innerHeight);
-    if (
-      ( window.innerWidth <= 600 && window.innerHeight <= 670 ) || 
-      ( window.innerHeight >= 600 && window.innerHeight < 660 )
-    ) {
+    if ( window.innerHeight <= 670 ) {
       //if not, redirect user
       //calculate login target
       let target = window.location.href;
