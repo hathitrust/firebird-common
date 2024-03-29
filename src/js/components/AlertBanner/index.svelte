eea3f83e041a84e81b02f086eb21f22fc04230ed
Caryl Wyatt <carylw@hathitrust.org>
2024-03-29 10:30:40 -0500

    update styles to match design

--- src/js/components/AlertBanner/index.svelte
+++ src/js/components/AlertBanner/index.svelte
@@ -1,28 +1,62 @@
-<div class="alert alert-warning d-flex mx-3" role="alert">
-  <!-- <i class="fa-solid fa-triangle-exclamation"></i> -->
+<script>
+  export let alertType = 'warning';
+</script>
+
+<div
+  class="alert d-flex mx-3 gap-2 {alertType === 'warning'
+    ? 'alert-warning'
+    : alertType === 'danger'
+      ? 'alert-danger'
+      : 'alert-info'}"
+  role="alert"
+>
+  <i class="alert-icon fa-solid fa-triangle-exclamation"></i>
   <div class="d-flex flex-column gap-2 py-3">
     <p class="alert-heading">Outage: Incomplete search results</p>
     <p>Users searching within the full text of all volumes will receive incomplete search results.</p>
     <a class="alert-link" href="https://www.hathitrust.org/press-post/outage-incomplete-search-results/"
-      >More Information</a
+      >More information</a
     >
   </div>
 </div>
 
-<style>
+<style lang="scss">
+  .alert-warning {
+    --bs-alert-color: var(--color-neutral-800);
+    --bs-alert-border-color: #997404;
+  }
+  .alert-danger {
+    --bs-alert-color: var(--color-neutral-800);
+    --bs-alert-border-color: #b02a37;
+  }
   .alert {
-    border-inline-start: 0.25rem solid var(--bs-alert-color);
-    padding: 0 1rem;
-  }
-  .alert,
-  .alert a {
-    color: var(--color-neutral-800);
-  }
-  .alert a.alert-link {
-    font-weight: 600;
-  }
-  p {
-    margin-block-end: 0;
+    border: none;
+    border-inline-start: 0.25rem solid var(--bs-alert-border-color);
+    padding: 0;
+    border-radius: 0.25rem;
+    box-shadow: 0px 4px 8px 0px rgba(25, 11, 1, 0.04);
+    i {
+      color: var(--bs-alert-border-color);
+      display: flex;
+      width: 1.5rem;
+      padding-block-start: 1rem;
+      flex-direction: column;
+      align-items: center;
+      gap: 0.5rem;
+      align-self: stretch;
+      margin-inline-start: 0.5rem;
+      line-height: 1.3125rem;
+    }
+    p,
+    a {
+      line-height: 1.3125rem;
+      letter-spacing: -0.01rem;
+      margin-block-end: 0;
+    }
+    a.alert-link {
+      font-weight: 500;
+      color: var(--bs-alert-color);
+    }
   }
   .alert-heading {
     font-weight: 700;
