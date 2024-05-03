53cba4246dd0e91ffe6ab5c0add424215d0027a1
Caryl Wyatt <carylw@hathitrust.org>
2024-05-03 16:24:45 -0500

    remove tests

--- src/js/lib/cookies.js
+++ src/js/lib/cookies.js
@@ -92,37 +92,37 @@ export const docCookies = {
 let expires = new Date();
 expires.setMonth(expires.getMonth() + 12);
 
-export function setCookieConsentSeen() {
+function setCookieConsentSeen() {
   docCookies.setItem('HT-cookie-banner-seen', 'true', expires, '/', HT.cookies_domain, true);
   cookieConsentSeen.set('true');
 }
-export function setTrackingAllowedCookie() {
+function setTrackingAllowedCookie() {
   docCookies.setItem('HT-tracking-cookie-consent', 'true', expires, '/', HT.cookies_domain, true);
   trackingConsent.set('true');
   return true;
 }
 
-export function setTrackingDisallowedCookie() {
+function setTrackingDisallowedCookie() {
   docCookies.setItem('HT-tracking-cookie-consent', 'false', expires, '/', HT.cookies_domain, true);
   trackingConsent.set('false');
 }
 
-export function setMarketingAllowedCookie() {
+function setMarketingAllowedCookie() {
   docCookies.setItem('HT-marketing-cookie-consent', 'true', expires, '/', HT.cookies_domain, true);
   marketingConsent.set('true');
 }
 
-export function setMarketingDisallowedCookie() {
+function setMarketingDisallowedCookie() {
   docCookies.setItem('HT-marketing-cookie-consent', 'false', expires, '/', HT.cookies_domain, true);
   marketingConsent.set('false');
 }
 
-export function setPreferencesAllowedCookie() {
+function setPreferencesAllowedCookie() {
   docCookies.setItem('HT-preferences-cookie-consent', 'true', expires, '/', HT.cookies_domain, true);
   preferencesConsent.set('true');
 }
 
-export function setPreferencesDisallowedCookie() {
+function setPreferencesDisallowedCookie() {
   docCookies.setItem('HT-preferences-cookie-consent', 'false', expires, '/', HT.cookies_domain, true);
   preferencesConsent.set('false');
 }
--- src/js/lib/cookies.test.js
+++ src/js/lib/cookies.test.js
@@ -1,7 +1,7 @@
 import { afterEach, afterAll, describe, it, expect, test, vi } from 'vitest'
-import { TestCookieJar, docCookies, setCookieConsentSeen, setMarketingAllowedCookie, setTrackingAllowedCookie, setTrackingDisallowedCookie, setMarketingDisallowedCookie, setPreferencesAllowedCookie, setPreferencesDisallowedCookie, setSelectedConsent} from './cookies'
+import { docCookies, setSelectedConsent} from './cookies'
 import { cookieConsentSeen, trackingConsent, marketingConsent, preferencesConsent, allowTracking } from './store'
-import { writable, get } from 'svelte/store';
+import { get } from 'svelte/store';
 
 // @vitest-environment happy-dom
 // happy-dom gives us browser stuff like window and document functions
@@ -25,25 +25,6 @@ it('should not be undefined', () => {
            expect(document).not.toBeUndefined() 
         })
 })
-describe('TestCookieJar', () => {
-    const mockCookie = {'COOKIE': 'true'}
-    const cookieJar = new TestCookieJar(mockCookie) 
-
-    describe('getItem', () => {
-        it('should return the value of the cookie', () => {
-            const trueCookie = cookieJar.getItem('COOKIE')
-            expect(trueCookie).toBe('true')
-            expect(trueCookie).not.toBe(true)
-        })
-    })
-    describe('setItem', () => { 
-        it('should set the cookie value', () => {
-            cookieJar.setItem('COOKIE', 'false')            
-            expect(cookieJar.data.COOKIE).toBe('false')
-            expect(cookieJar.data.COOKIE).not.toBe(false)
-        })
-    })
-})
 describe('docCookies', () => {
     afterEach(() => {
         document.cookie = "COOKIE=true;expires=Thu, 01 Jan 1970 00:00:00 GMT"
@@ -63,7 +44,6 @@ describe('docCookies', () => {
     })
     describe('setItem', () => {
         test('sets cookie with key and value', () => {
-            // expect(docCookies.setItem('COOKIE', 'cookie')).toBe(true)
             expect(docCookies.setItem('COOKIE', 'cookie', '', '', '')).toBe(true)
             expect(document.cookie).toBeTruthy()
             expect(document.cookie).toContain("COOKIE=cookie")
@@ -84,87 +64,87 @@ describe('docCookies', () => {
         })
     })
 })
-describe('setCookieConsentSeen', () => {
-    afterAll(() => {
-        docCookies.removeItem('HT-cookie-banner-seen');
-    }) 
-    test('cookieConsentSeen should be undefined', () => {
-        expect(get(cookieConsentSeen)).toBeUndefined()
-    })
+// describe('setCookieConsentSeen', () => {
+//     afterAll(() => {
+//         docCookies.removeItem('HT-cookie-banner-seen');
+//     }) 
+//     test('cookieConsentSeen should be undefined', () => {
+//         expect(get(cookieConsentSeen)).toBeUndefined()
+//     })
 
-    it('sets HT-cookie-banner-seen cookie', () => {
+//     it('sets HT-cookie-banner-seen cookie', () => {
     
-        setCookieConsentSeen()
-        expect(setItemSpy).toHaveBeenCalled()
-        expect(document.cookie).not.toEqual('')
-        expect(document.cookie).toContain("HT-cookie-banner-seen=true")
+//         setCookieConsentSeen()
+//         expect(setItemSpy).toHaveBeenCalled()
+//         expect(document.cookie).not.toEqual('')
+//         expect(document.cookie).toContain("HT-cookie-banner-seen=true")
 
-    })
-    it('sets cookieConsentSeen store variable to true', () => {
-        expect(get(cookieConsentSeen)).toBe('true')
-    })
+//     })
+//     it('sets cookieConsentSeen store variable to true', () => {
+//         expect(get(cookieConsentSeen)).toBe('true')
+//     })
     
-})
-describe('setTrackingAllowedCookie', () => {
-    afterAll(() => {
-        docCookies.removeItem('HT-tracking-cookie-consent');
-    }) 
-    test('trackingConsent should be false', () => {
-        expect(get(trackingConsent)).toBe('false')
-    })
-    it('sets trackingConsent to true', () => {
-        setTrackingAllowedCookie()
-        expect(get(trackingConsent)).toBe('true')
-    })
-})
+// })
+// describe('setTrackingAllowedCookie', () => {
+//     afterAll(() => {
+//         docCookies.removeItem('HT-tracking-cookie-consent');
+//     }) 
+//     test('trackingConsent should be false', () => {
+//         expect(get(trackingConsent)).toBe('false')
+//     })
+//     it('sets trackingConsent to true', () => {
+//         setTrackingAllowedCookie()
+//         expect(get(trackingConsent)).toBe('true')
+//     })
+// })
 
-describe('setTrackingDisallowedCookie', () => {
-    it('sets trackingConsent to false', () => {
-        setTrackingDisallowedCookie()
-        expect(get(trackingConsent)).toBe('false')
-    })
-})
+// describe('setTrackingDisallowedCookie', () => {
+//     it('sets trackingConsent to false', () => {
+//         setTrackingDisallowedCookie()
+//         expect(get(trackingConsent)).toBe('false')
+//     })
+// })
 
-describe('setMarketingAllowedCookie', () => {
-    test('marketingConsent should be false', () => {
-        expect(get(marketingConsent)).toBe('false')
-    })
-    it('sets marketingConsent to true', () => {
-        setMarketingAllowedCookie()
-        expect(get(marketingConsent)).toBe('true')
-    })
-})
-describe('setMarketingDisallowedCookie', () => {
-    it('sets marketingConsent to false', () => {
-        setMarketingDisallowedCookie()
-        expect(get(marketingConsent)).toBe('false')
-    })
-})
-describe('setPreferencesAllowedCookie', () => {
-    test('preferencesConsent should be false', () => {
-        expect(get(preferencesConsent)).toBe('false')
-    })
-    it('sets preferenecsConsent to true', () => {
-        setPreferencesAllowedCookie()
-        expect(get(preferencesConsent)).toBe('true')
-    })
-})
-describe('setPreferencesDisallowedCookie', () => {
-    it('sets preferenecsConsent to false', () => {
-        setPreferencesDisallowedCookie()
-        expect(get(preferencesConsent)).toBe('false')
-    })
-})
+// describe('setMarketingAllowedCookie', () => {
+//     test('marketingConsent should be false', () => {
+//         expect(get(marketingConsent)).toBe('false')
+//     })
+//     it('sets marketingConsent to true', () => {
+//         setMarketingAllowedCookie()
+//         expect(get(marketingConsent)).toBe('true')
+//     })
+// })
+// describe('setMarketingDisallowedCookie', () => {
+//     it('sets marketingConsent to false', () => {
+//         setMarketingDisallowedCookie()
+//         expect(get(marketingConsent)).toBe('false')
+//     })
+// })
+// describe('setPreferencesAllowedCookie', () => {
+//     test('preferencesConsent should be false', () => {
+//         expect(get(preferencesConsent)).toBe('false')
+//     })
+//     it('sets preferenecsConsent to true', () => {
+//         setPreferencesAllowedCookie()
+//         expect(get(preferencesConsent)).toBe('true')
+//     })
+// })
+// describe('setPreferencesDisallowedCookie', () => {
+//     it('sets preferenecsConsent to false', () => {
+//         setPreferencesDisallowedCookie()
+//         expect(get(preferencesConsent)).toBe('false')
+//     })
+// })
 describe('setSelectedConsent', () => {
     afterAll(() => {
         docCookies.removeItem('HT-tracking-cookie-consent')
     })
-    it.skip('should use setTrackingAllowedCookie', () => {
+    // it.skip('should use setTrackingAllowedCookie', () => {
         // const mockSetTrackingAllowedCookie = vi.fn().mockImplementation('setTrackingAllowedCookie')
         //not sure how to find out if it used this function
         // setSelectedConsent()
 
-    }) 
+    // }) 
     it('should set HT-tracking-consent to true', () => {
         allowTracking.set(true);
         setSelectedConsent()
