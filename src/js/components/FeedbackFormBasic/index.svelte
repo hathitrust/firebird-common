b1b531149a0bb0e1553404dd810510503a992caa
Caryl Wyatt <carylw@hathitrust.org>
2024-09-26 10:48:19 -0400

    migration to svelte 4

--- src/js/components/FeedbackFormBasic/index.svelte
+++ src/js/components/FeedbackFormBasic/index.svelte
@@ -227,7 +227,7 @@
   {#if submitted}
     <section>
       {#if postResponseStatusCode === 200}
-        <div transition:slide>
+        <div transition:slide|global>
           <div class="alert alert-success submit-message" role="alert">
             <i class="fa-solid fa-circle-check fa-lg me-2" />
             <div>
@@ -241,7 +241,7 @@
           </div>
         </div>
       {:else if postResponseStatusCode === 429}
-        <div transition:slide>
+        <div transition:slide|global>
           <div class="alert alert-danger submit-message" role="alert">
             <i class="fa-solid fa-triangle-exclamation fa-lg me-2" />
             <div>
@@ -251,7 +251,7 @@
           </div>
         </div>
       {:else}
-        <div transition:slide={{ duration: 300 }}>
+        <div transition:slide|global={{ duration: 300 }}>
           <div class="alert alert-danger submit-message" role="alert">
             <i class="fa-solid fa-triangle-exclamation fa-lg me-2" />
             <div>
