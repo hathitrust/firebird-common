e392514a12b362acac06ba9e51145ce704804505
Caryl Wyatt <carylw@hathitrust.org>
2024-10-03 12:39:03 -0400

    fiddle with storybook config

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,7 @@ const config = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    // '@storybook/addon-designs',
+    '@storybook/addon-designs',
   ],
   framework: {
     name: '@storybook/svelte-vite',
