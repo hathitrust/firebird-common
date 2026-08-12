b39ebd0e641b1b721842c44b0fa51c3aa3e72f71
Caryl Wyatt <carylw@hathitrust.org>
2026-08-12 16:42:04 -0400

    add styles

--- src/js/components/CopySnippet/index.svelte
+++ src/js/components/CopySnippet/index.svelte
@@ -1,29 +1,23 @@
 <script>
+  let { snippetLink = null } = $props();
 
-let {
-    snippetLink = null,
-} = $props();
+  let snippet;
+  let tooltip = $state();
+  let tooltipOpen = $state(false);
 
-let snippet;
-let tooltip = $state();
-let tooltipOpen = $state(false);
-
-async function copyCode(snippet, tooltip) {
-
+  async function copyCode(snippet, tooltip) {
     await navigator.clipboard.writeText(snippet);
 
     //updates tooltip text
     tooltip.innerHTML =
-        '<span aria-live="polite">Copied</span> <i class="fa-solid fa-circle-check" aria-hidden="true"></i>';
+      '<span aria-live="polite">Copied</span> <i class="fa-solid fa-circle-check" aria-hidden="true"></i>';
   }
 
   function onmouseleave(e) {
-    // console.log('mouseleave copy button', e.target.lastElementChild);
     e.target.lastElementChild.innerText = 'Copy';
     tooltip.removeAttribute('style');
   }
   function onblur(e) {
-    // console.log('mouseleave copy button', e.target.lastElementChild);
     e.target.lastElementChild.innerText = 'Copy';
     tooltip.removeAttribute('style');
   }
@@ -38,34 +32,32 @@ async function copyCode(snippet, tooltip) {
   }
 </script>
 
- <div class="d-flex gap-3">
-    <figure class="snippet">
-        <div class="d-flex">
-        <pre tabindex="-1"><code bind:this={snippet}
-            >{snippetLink}</code
-            ></pre>
-        <button
-            type="button"
-            class="copycode"
-            title="Copy to clipboard"
-            onclick={() => copyCode(snippetLink, tooltip)}
-            onfocus={() => ((tooltipOpen = true), console.log('tooltip?', tooltipOpen))}
-            {onblur}
-            {onmouseleave}
+<div class="d-flex gap-3">
+  <figure class="snippet">
+    <div class="d-flex">
+      <pre tabindex="-1"><code bind:this={snippet}>{snippetLink}</code></pre>
+      <button
+        type="button"
+        class="copycode"
+        title="Copy to clipboard"
+        onclick={() => copyCode(snippetLink, tooltip)}
+        onfocus={() => (tooltipOpen = true)}
+        {onblur}
+        {onmouseleave}
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" class="copy-icon" viewBox="0 0 448 512"
+          ><path
+            d="M384 336l-192 0c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l140.1 0L400 115.9 400 320c0 8.8-7.2 16-16 16zM192 384l192 0c35.3 0 64-28.7 64-64l0-204.1c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1L192 0c-35.3 0-64 28.7-64 64l0 256c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-32-48 0 0 32c0 8.8-7.2 16-16 16L64 464c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l32 0 0-48-32 0z"
+          ></path></svg
         >
-            <svg xmlns="http://www.w3.org/2000/svg" class="copy-icon" viewBox="0 0 448 512"
-            ><path
-                d="M384 336l-192 0c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l140.1 0L400 115.9 400 320c0 8.8-7.2 16-16 16zM192 384l192 0c35.3 0 64-28.7 64-64l0-204.1c0-12.7-5.1-24.9-14.1-33.9L366.1 14.1c-9-9-21.2-14.1-33.9-14.1L192 0c-35.3 0-64 28.7-64 64l0 256c0 35.3 28.7 64 64 64zM64 128c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-32-48 0 0 32c0 8.8-7.2 16-16 16L64 464c-8.8 0-16-7.2-16-16l0-256c0-8.8 7.2-16 16-16l32 0 0-48-32 0z"
-            ></path></svg
-            >
-            <tool-tip role="tooltip" bind:this={tooltip}>Copy</tool-tip>
-        </button>
-        </div>
-    </figure>
+        <tool-tip role="tooltip" bind:this={tooltip}>Copy</tool-tip>
+      </button>
+    </div>
+  </figure>
 </div>
 
 <style lang="scss">
-    tool-tip {
+  tool-tip {
     opacity: 0;
     visibility: hidden;
     z-index: 1;
