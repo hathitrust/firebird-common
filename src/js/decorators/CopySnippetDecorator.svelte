44fe7cd433232a1847a8d044c5e4a02b1c552b6a
Caryl Wyatt <carylw@hathitrust.org>
2026-08-13 15:26:01 -0500

    ETT-1484: copy snippet component, add new utility classes (#154)
    * add copy snippet component
    
    * add styles
    
    * add esc handling
    
    * add story decorator and interaction test
    
    * clean up
    
    * remove invalid css

---
+++ src/js/decorators/CopySnippetDecorator.svelte
@@ -0,0 +1,15 @@
+<script>
+  let { children } = $props();
+</script>
+
+<div>
+  <div class="snippet-decorator">
+    {@render children()}
+  </div>
+</div>
+
+<style>
+  .snippet-decorator {
+    margin: 3em 0;
+  }
+</style>
